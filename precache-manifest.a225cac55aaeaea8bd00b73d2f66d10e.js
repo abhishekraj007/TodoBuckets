87fd76a0bf4fd7a22669b484f88937e0a54bb9b1
akrj <abhishek@lollypop.design>
2019-11-24 13:57:31 +0530

    Updates

---
+++ precache-manifest.a225cac55aaeaea8bd00b73d2f66d10e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2622eea5672cd760b090a9a870c441",
+    "url": "/TodoBuckets/index.html"
+  },
+  {
+    "revision": "539186ba7afdf5a78e4a",
+    "url": "/TodoBuckets/static/css/main.a3d4fdbb.chunk.css"
+  },
+  {
+    "revision": "8c7baba98c825b9c5f11",
+    "url": "/TodoBuckets/static/js/2.11108588.chunk.js"
+  },
+  {
+    "revision": "539186ba7afdf5a78e4a",
+    "url": "/TodoBuckets/static/js/main.08a90b3c.chunk.js"
+  },
+  {
+    "revision": "5d0d9672692d7e3129f6",
+    "url": "/TodoBuckets/static/js/runtime-main.94708019.js"
+  }
+]);
